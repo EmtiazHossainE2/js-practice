9c7f896a535ea13a88776470a2bf46ae3604fda2
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-30 15:09:24 +0600

    leap year code

---
+++ leap-year.js
@@ -0,0 +1,9 @@
+function isLeapYear (year){
+    if(year % 4 == 0 && year % 100 != 0){
+        return true ;
+    }
+    return false;
+}
+const myYear = 2030 ; 
+const isMyYearLeapYear = isLeapYear(myYear)
+console.log('Is my year is leap year ', isMyYearLeapYear);
