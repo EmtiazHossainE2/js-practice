9a51e72574e4ae10c29878b6d2346f2ae5c5eb04
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-30 18:57:19 +0600

    factorial using function

--- factorial.js
+++ factorial.js
@@ -1,5 +1,10 @@
-let factorial = 1
-for(let i = 1; i <= 6; i++){
-    factorial = factorial * i ; 
-    console.log(factorial);
+function getFactorial (number){
+    let factorial = 1
+    for(let i = 1; i <= number; i++){
+        factorial = factorial * i ; 
+    }
+    return factorial;
 }
+let number = 8 ; 
+let factorialResult = getFactorial(number)
+console.log('Your factorial value is : ', factorialResult);
