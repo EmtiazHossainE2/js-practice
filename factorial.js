daa234e67b102f982fd5facb89a52f89a6082ff0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-30 20:56:56 +0600

    factorial learning

--- factorial.js
+++ factorial.js
@@ -1,10 +1,23 @@
+// function getFactorial (number){
+//     let factorial = 1
+//     for(let i = 1; i <= number; i++){
+//         factorial = factorial * i ; 
+//     }
+//     return factorial;
+// }
+// let number = 8 ; 
+// let factorialResult = getFactorial(number)
+// console.log('Your factorial value is : ', factorialResult);
+
 function getFactorial (number){
-    let factorial = 1
-    for(let i = 1; i <= number; i++){
-        factorial = factorial * i ; 
+    let factorial = 1 ; 
+    let i = 1 ; 
+    while(i<=number){
+        factorial = factorial * i ;
+        i++
     }
-    return factorial;
+    return factorial 
 }
-let number = 8 ; 
+let number = 15 ;
 let factorialResult = getFactorial(number)
 console.log('Your factorial value is : ', factorialResult);
