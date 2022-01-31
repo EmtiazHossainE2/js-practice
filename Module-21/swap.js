79c979cd9ebef7a180724e75da76e42cbeadca7e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-31 23:52:27 +0600

    swap js

---
+++ Module-21/swap.js
@@ -0,0 +1,7 @@
+let num1 = 1 ; 
+let num2 = 2 ; 
+let num3 = 3 ; 
+console.log(num1, num2, num3);
+// [num1, num2, num3] = [num2, num3, num1 ]
+[num1, num2, num3] = [num2, num1, num3 ] //ektar value onno take dicci 
+console.log(num1, num2, num3);
