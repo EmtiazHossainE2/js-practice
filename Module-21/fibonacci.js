7066316b345cdfb49ba70758555b6e2198a022a7
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-01 23:45:02 +0600

    error handling

--- Module-21/fibonacci.js
+++ Module-21/fibonacci.js
@@ -1,11 +1,17 @@
-function dhara (){
+function dhara (num){
+    if(typeof num != 'number'){
+        return 'Enter a  number'
+    }
+    if(num < 2 ){
+        return 'Enter a positive number greater than 1'
+    }
     let fibonacci = [0,1]
-    for(let i = 2 ; i<=15 ; i++){
+    for(let i = 2 ; i<=num ; i++){
     //nth = (n-1)th + (n-2)th
     fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]
     }
     return fibonacci;
 }
-let result = dhara()
+let result = dhara(-10) ;
 console.log(result);
 
