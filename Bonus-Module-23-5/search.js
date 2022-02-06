f3d5fa6ce6774f0d4043100f8bcd8db997903154
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-06 21:15:40 +0600

    test

--- Bonus-Module-23-5/search.js
+++ Bonus-Module-23-5/search.js
@@ -27,21 +27,3 @@ function compare(a, b) {
     }
 }
 
-const result = compare(25, 25);
-console.log(result);
-
-console.log("123" + 123);
-
-let a = 'hi'
-let b = 'there'
-console.log(a + b)
-
-const array = [1, 2, 3, 4, 5, 6, 7];
-
-
-for (let i = 1; i < array.length; i++) {
-    if (i == 5) {
-        continue;
-    }
-    console.log(array[i]);
-}
