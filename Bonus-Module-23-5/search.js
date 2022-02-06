31ffaef70b874857634bd1809eec349102653926
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-06 20:31:20 +0600

    test

--- Bonus-Module-23-5/search.js
+++ Bonus-Module-23-5/search.js
@@ -1,45 +1,47 @@
 const products = [
-    {name : 'Hp Laptop' , price : 55000} ,
-    {name : 'Asus Laptop' , price : 85000} ,
-    {name : 'I phone 17' , price : 505000} ,
-    {name : 'Watch apple' , price : 55000} ,
-    {name : 'Tab apple' , price : 55000} ,
-    {name : 'Laptop Xd' , price : 59000} 
+    { name: 'Hp Laptop', price: 55000 },
+    { name: 'Asus Laptop', price: 85000 },
+    { name: 'I phone 17', price: 505000 },
+    { name: 'Watch apple', price: 5000 },
+    { name: 'Tab apple', price: 55000 },
+    { name: 'Watch', price: 4000 },
+    { name: 'Laptop Xd', price: 59000 }
 ]
-function searchProducts (products ,searchText){
+function searchProducts(products, searchText) {
     let result = []
-    for(const product of products){
-        if(product.name.includes(searchText)){
+    for (const product of products) {
+        if (product.name.includes(searchText)) {
             result.push(product)
         }
     }
-    return result ;
+    return result;
 }
-console.log(searchProducts(products ,'Laptop')) ;
+console.log(searchProducts(products, 'Laptop'));
 
 
 function compare(a, b) {
     if (a.toString() === b) {
-     return true;
+        return true;
     } else {
-     return false;
+        return false;
     }
-   }
-   
-   const result = compare(25, 25);
-   console.log(result);
+}
 
-   console.log("123" + 123);
+const result = compare(25, 25);
+console.log(result);
 
-   let a='hi'
-let b='there'
-console.log(a+b)
+console.log("123" + 123);
+
+let a = 'hi'
+let b = 'there'
+console.log(a + b)
 
 const array = [1, 2, 3, 4, 5, 6, 7];
 
+
 for (let i = 1; i < array.length; i++) {
- if (i == 5) {
-  continue;
- }
- console.log(array[i]);
+    if (i == 5) {
+        continue;
+    }
+    console.log(array[i]);
 }
